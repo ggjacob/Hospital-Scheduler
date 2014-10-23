11856f47dd3b82e1e4186f290d4f3adb739eeca8
kgranch <ksg56@nau.edu>
2014-10-22 21:32:38 -0700

    Update getShiftData.php
    Prelim query

--- php/getShiftData.php
+++ php/getShiftData.php
@@ -2,7 +2,7 @@
 include 'SessionLogin.php';
 
 /*TODO: Input the MySQL query to pull all of the shift data. The code below should take care of the rest for you. */
-$sql = ("     ");
+$sql = ("SELECT DISTINCT Shift_ID, Mon, Tues, Wed, Thurs, Fri, Sat, Sun, Shift_Begin, Shift_End FROM schedule;");
 
 $result = $mysqli->query($sql) or die($mysqli->error);
 $array_result = array();
