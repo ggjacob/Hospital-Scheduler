b862a7c9210ba4237d9c9f173713c9b1a6b210b0
Duke Ayers <Dfa24@nau.edu>
2014-10-22 22:26:17 -0700

    Split navbar
    Put navbar in the headSection.php file to make it stay consistent across
    all pages.

--- calendarView.php
+++ calendarView.php
@@ -1,59 +1,6 @@
 <?php
 include_once "php/headSection.php";
 ?>
-<!--The head section can be found in the headSection.php file-->
-<body  ng-app="calendarApp" ng-controller="controller" ng-init="getCalendarData()">
-<nav class="navbar navbar-default" role="navigation">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#get_data-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"></a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="get_data-1">
-            <ul class="nav navbar-nav">
-                <li><a href="#">Home Page</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Calendar<span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Weekly</a></li>
-                        <li><a href="#">Monthly</a></li>
-                        <li><a href="#">Yearly</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Scheduler<span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Add Appointment</a></li>
-                        <li><a href="#">Remove Appointment</a></li>
-                        <li><a href="#">Change Appointment</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admins Only<span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Add Employee</a></li>
-                        <li><a href="#">Remove Employee</a></li>
-                        <li><a href="#">Change Employee</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Log Out</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-
-
-
 <div class="container">
     <div class="row text-center">
         <h3 style="border-bottom:1px solid lightgray">Calendar View</h3>
--- php/headSection.php
+++ php/headSection.php
@@ -8,14 +8,62 @@
 <head lang="en" >
     <meta charset="UTF-8">
     <title></title>
-
+    <link href="/Hospital-Scheduler/css/master.css" type="text/css" rel="stylesheet"/>
+    <link href="/Hospital-Scheduler/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
+    <script src="/Hospital-Scheduler/js/jquery.min.js"></script>
+    <script src="/Hospital-Scheduler/js/bootstrap.min.js"></script>
+    <!--Loading the angular from the website, I am having problems with getting it to load-->
+    <script src="//code.angularjs.org/1.2.12/angular.js"></script>
+    <script src="/Hospital-Scheduler/js/Controllers/controller.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
+    <script src="/Hospital-Scheduler/js/master.js"></script>
 </head>
-<link href="/Hospital-Scheduler/css/master.css" type="text/css" rel="stylesheet"/>
-<link href="/Hospital-Scheduler/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
-<script src="/Hospital-Scheduler/js/jquery.min.js"></script>
-<script src="/Hospital-Scheduler/js/bootstrap.min.js"></script>
-<!--Loading the angular from the website, I am having problems with getting it to load-->
-<script src="//code.angularjs.org/1.2.12/angular.js"></script>
-<script src="/Hospital-Scheduler/js/Controllers/controller.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular-route.js"></script>
-<script src="/Hospital-Scheduler/js/master.js"></script>
+<body  ng-app="calendarApp" ng-controller="controller" ng-init="getCalendarData()">
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#get_data-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="get_data-1">
+            <ul class="nav navbar-nav">
+                <li><a href="calendarView.php">Home Page</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Calendar<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Weekly</a></li>
+                        <li><a href="#">Monthly</a></li>
+                        <li><a href="#">Yearly</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Scheduler<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Add Appointment</a></li>
+                        <li><a href="#">Remove Appointment</a></li>
+                        <li><a href="#">Change Appointment</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admins Only<span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Add Employee</a></li>
+                        <li><a href="#">Remove Employee</a></li>
+                        <li><a href="#">Change Employee</a></li>
+                    </ul>
+                </li>
+                <li><a href="shiftView.php">Shift View</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Log Out</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
