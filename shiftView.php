4a3f506a657f9f2361b41f2e0d9b697fc4ef8552
Ryan Dooley <rcd53@nau.edu>
2014-11-03 12:55:06 -0700

    Added Shift Details Modal
    This contains a button that pops up a modal containing a form to change
    the shift details. Not connected to back end, So it does not display
    details of specific shifts and cannot actually update yet.

--- shiftView.php
+++ shiftView.php
@@ -28,7 +28,45 @@ include_once "php/navBar.php";
                 </tr>
             </tbody>
         </table>
+
     </div>
+
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-default" data-toggle="modal" data-target="#myModal">
+        Details
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Shift Details</h4>
+                </div>
+                <div class="modal-body">
+                    This is where the details of the shift will go.
+                </div>
+                <div class="modal-body">
+                    <form role="form">
+                        <div class="form-group">
+                            <label for="shiftstart">Shift Start:</label>
+                            <input type="text" class="form-control" id="shiftstart" placeholder="Enter Shift Start Time">
+                        </div>
+                        <div class="form-group">
+                            <label for="shiftend">Shift End:</label>
+                            <input type="text" class="form-control" id="shiftend" placeholder="Enter Shift End Time">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <!--    <div class="row" >-->
 <!--        <div class="col-lg-4">-->
 <!--            {{data.ID}}-->
