d596eaf1c83c609f4a5796022ff27dc5eb696295
Ryan Dooley <rcd53@nau.edu>
2014-11-03 20:56:58 -0700

    Add New Shift Modal
    Added a modal to appear when the Add New Shift Button is clicked.

--- shiftView.php
+++ shiftView.php
@@ -12,7 +12,7 @@ include_once "php/navBar.php";
     </div>
 
     <div class="row">
-        <div class="col-sm-6 col-sm-offset-1">
+        <div class="col-sm-9">
             <div class="table-responsive text-center">
                 <table class="table table-striped">
                     <thead>
@@ -32,8 +32,8 @@ include_once "php/navBar.php";
                 </table>
             </div>
         </div>
-        <div class="col-sm-4">
-            <button class="btn btn-transparent" style="width:100%; font-weight:800;">Add New Shift</button>
+        <div class="col-sm-3">
+            <button class="btn btn-transparent" data-toggle="modal" data-target="#newShift" style="width:100%; font-weight:800;">Add New Shift</button>
             <button class="btn btn-transparent" style="width:100%; font-weight:800; margin-top:10px;">Delete Shift</button>
         </div>
     </div>
@@ -42,6 +42,37 @@ include_once "php/navBar.php";
 
     </div>
 
+    <!--Modal for Adding a shift -->
+    <div class="modal fade" id="newShift" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Add New Shift</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-success text-center" ng-show="successMessage" role="alert">
+                        <h3>{{successMessage}}</h3>
+                    </div>
+                    <form ng-submit="updateShifts()" role="form">
+                        <div class="form-group">
+                            <label for="shiftbegin">Shift Start:</label>
+                            <input type="text" class="form-control" ng-model="formData.shiftStart" id="shiftbegin" placeholder="Enter Shift Start Time">
+                        </div>
+                        <div class="form-group">
+                            <label for="shiftclose">Shift End:</label>
+                            <input type="text" class="form-control" ng-model="formData.shiftEnd" id="shiftclose" placeholder="Enter Shift End Time">
+                        </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Modal for Shift Updates -->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
