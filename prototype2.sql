0e56f477f16aad2054e2ab6231a5306ea1279a16
Duke Ayers <Dfa24@nau.edu>
2014-11-08 13:48:02 -0700

    Updated Prototype2.sql
    Updated the database to a few changes made in the validation techniques.

--- prototype2.sql
+++ prototype2.sql
@@ -3,12 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2014 at 08:08 AM
+-- Generation Time: Nov 08, 2014 at 09:46 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "-07:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -45,6 +45,27 @@ INSERT INTO `employee` (`EMPLOYEE_ID`, `EMPLOYEE_FNAME`, `EMPLOYEE_LNAME`) VALUE
 (54321, 'Ekud', 'Lastname'),
 (68794, 'Bob', 'McSwag'),
 (78456, 'Timothy', 'Bear');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faq`
+--
+
+CREATE TABLE IF NOT EXISTS `faq` (
+  `ID` int(20) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(300) NOT NULL,
+  `Body` varchar(3000) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `faq`
+--
+
+INSERT INTO `faq` (`ID`, `Title`, `Body`) VALUES
+(1, 'What Browser Should I Use?', 'Any of the most up-to-date browsers are going to be the best to use with the scheduling application. This includes and up-to-date version of Google Chrome, Mozilla Firefox, or Internet Explorer'),
+(2, 'How Do I Add A Unique Shift?', 'This is currently being built but you should be able to just open up the indicated page and submit the new unique shift for all employees to use.');
 
 -- --------------------------------------------------------
 
@@ -87,8 +108,8 @@ INSERT INTO `schedule` (`SCHEDULE_SHIFT_ID`, `SCHEDULE_EMPLOYEE_ID`, `SCHEDULE_D
 
 CREATE TABLE IF NOT EXISTS `shift` (
   `SHIFT_ID` int(2) NOT NULL,
-  `SHIFT_START` int(20) NOT NULL,
-  `SHIFT_END` int(20) NOT NULL,
+  `SHIFT_START` varchar(20) NOT NULL,
+  `SHIFT_END` varchar(20) NOT NULL,
   PRIMARY KEY (`SHIFT_ID`),
   KEY `SHIFT_ID` (`SHIFT_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -98,11 +119,13 @@ CREATE TABLE IF NOT EXISTS `shift` (
 --
 
 INSERT INTO `shift` (`SHIFT_ID`, `SHIFT_START`, `SHIFT_END`) VALUES
-(1, 730, 1600),
-(2, 800, 1400),
-(3, 1200, 2350),
-(4, 1600, 2350),
-(5, 200, 900);
+(1, '04:00', '06:00'),
+(2, '08:00', '14:00'),
+(3, '12:00', '23:50'),
+(4, '16:00', '23:50'),
+(5, '02:00', '09:00'),
+(56, '12:30', '15:30'),
+(123, '12:30', '04:30');
 
 --
 -- Constraints for dumped tables
