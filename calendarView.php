9246733855661352288fc5f84cf042883592a426
Ryan Dooley <rcd53@nau.edu>
2014-11-12 23:51:35 -0700

    Added modals
    Added modals for requesting time off and shift details. When shift
    details is displayed it gives the option to switch shifts which displays
    another modal and asks for information.

--- calendarView.php
+++ calendarView.php
@@ -33,6 +33,95 @@ include_once "php/navBar.php";
         </div>
 </div>
 
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#shiftDetails">
+    Shift Details
+</button>
+
+<!-- Modal For Shift Details-->
+<div class="modal fade" id="shiftDetails" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">Shift Details</h4>
+            </div>
+            <div class="modal-body">
+                this will pull from the data base to display information regarding a persons shift.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#swapShift">Swap Shift</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal For Swapping Shifts -->
+<div class="modal fade" id="swapShift" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">Swap Shift</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="shiftDay">Date to swap:</label>
+                    <input type="text" class="form-control" ng-model="" id="shiftDay" placeholder="Date to swap shift">
+                </div>
+                <div class="form-group">
+                    <label for="person">Person to swap:</label>
+                    <input type="text" class="form-control" ng-model="" id="person" placeholder="Person to swap shift">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#timeOff">
+    Request Time Off
+</button>
+
+<!-- Modal For Requesting Time Off-->
+<div class="modal fade" id="timeOff" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">Shift Details</h4>
+            </div>
+            <div class="modal-body">
+                From:
+                <select>
+                    <option value="Monday">Monday</option>
+                    <option value="Monday">Tuesday</option>
+                    <option value="Monday">Wednesday</option>
+                    <option value="Monday">Thursday</option>
+                    <option value="Monday">Friday</option>
+                    <option value="Monday">Saturday</option>
+                    <option value="Monday">Sunday</option>
+                </select>
+                To:
+                <select>
+                    <option value="Monday">Monday</option>
+                    <option value="Monday">Tuesday</option>
+                    <option value="Monday">Wednesday</option>
+                    <option value="Monday">Thursday</option>
+                    <option value="Monday">Friday</option>
+                    <option value="Monday">Saturday</option>
+                    <option value="Monday">Sunday</option>
+                </select>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#swapShift">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!--The below calls two partial templates-->
 <div ng-include="'js/Template/dataTable.tpl.html'"></div>
